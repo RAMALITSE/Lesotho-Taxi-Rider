cbd684193bd03af19d19d0c7f78ab08947dd9202
Bafokeng Ramalitse <Drramalitse@gmail.com>
2022-06-08 19:39:26 +0200

    Adding Login Page

--- lib/All Screens/login.dart
+++ lib/All Screens/login.dart
@@ -8,22 +8,40 @@ class Login extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(children: [
-        SizedBox(
-          height: 45.0,
+        //box for logo image
+        const SizedBox(
+          height: 25.0,
         ),
-        Image(
+        const Image(
           image: AssetImage("images/logo.png"),
           width: 350.0,
           height: 200.0,
           alignment: Alignment.center,
         ),
-        SizedBox(
+
+        //box for text login box
+        const SizedBox(
           height: 1.0,
         ),
-        Text(
-          "Login as a Rider",
+        const Text(
+          "Login To Request A Car",
           style: TextStyle(fontSize: 24.0, fontFamily: "Brand-Bold"),
           textAlign: TextAlign.center,
+        ),
+
+        //box for textformfield
+        const SizedBox(
+          height: 1.0,
+        ),
+        TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(
+              labelText: "Email",
+              labelStyle: TextStyle(
+                fontSize: 14.0,
+                fontFamily: "Brand-Bold",
+              ),
+              hintStyle: TextStyle(color: Colors.grey)),
         ),
       ]),
     );
